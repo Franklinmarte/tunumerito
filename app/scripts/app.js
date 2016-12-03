3b614efdec483c24e540c7895f23be146cc0bcc8
Franklinmarte <franklinmarte@cloudnine.com.do>
2016-12-03 13:53:26 -0600

    aqui estoy iniciando, ya tengo el diseño que usare. solo falta funcionalidad

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name numeroApp
+ * @description
+ * # numeroApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('numeroApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name numeroApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the numeroApp
+ */
+angular.module('numeroApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
