fb9e2359661b648f085a704372ece474a1355503
Franklinmarte <franklinmarte@cloudnine.com.do>
2016-12-07 11:26:34 -0600

    ya genera el primer numero aleatorio sin guardarlo en la cookie

--- app/scripts/controllers/controller.js
+++ app/scripts/controllers/controller.js
@@ -4,7 +4,7 @@ var numeroApp = angular.module('numeroAppController',[]);
 numeroApp.controller('conteoCtrl',['$scope',function($scope){
 	$scope.hola = function()
 	{
-		 $scope.numero = Math.floor((Math.random() * 100) + 1);
+		 $scope.numero = parseInt(Math.floor((Math.random() * 100) + 1))
 		return $scope.numero;
 	}
 
